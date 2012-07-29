2672e0ef3326ce6f64bac22c1debdc2d22e46b29
Subito <ffheidt@gmail.com>
2012-07-29 20:46:13 +0200

    models work as expected.

--- updater/models.py
+++ updater/models.py
@@ -8,15 +8,15 @@ from django.contrib.auth.models import User
 class Host(models.Model):
     user = models.ForeignKey(User)
     name = models.CharField(max_length=255)
-    access_key = models.CharField(max_length=255)
+    access_key = models.CharField(max_length=255, blank=True)
     last_updated = models.DateTimeField(auto_now=True)
     added = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         if not self.access_key:
             md5 = hashlib.md5()
-            md5.update(datetime.now())
-            md5.update(name)
+            md5.update(str(datetime.now()))
+            md5.update(self.name)
             self.access_key = md5.hexdigest()
         super(Host, self).save(*args, **kwargs)
 
@@ -26,8 +26,10 @@ class Host(models.Model):
 
 class Update(models.Model):
     host = models.ForeignKey(Host)
-    from_ip = models.CharField() # Is there such thing as an "IP-Field"? Maybe write one?
+    from_ip = models.CharField(max_length=255) # Is there such thing as an "IP-Field"? Maybe write one?
     message = models.TextField(blank=True) # optional TextMessage, a client can provide
     added = models.DateTimeField(auto_now_add=True)
     in_zone = models.BooleanField(default=False)
     
+    def __unicode__(self):
+        return self.host.name
