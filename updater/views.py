a968418f9433d857977ec4f7d94784c1a1a307e0
Subito <ffheidt@gmail.com>
2012-07-29 20:45:46 +0200

    corrected some bugs, view works as expected

--- updater/views.py
+++ updater/views.py
@@ -2,9 +2,11 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 from updater.models import Host, Update
 
+@csrf_exempt
 def update(request):
     if request.method != 'POST':
         return HttpResponse('No data submitted')
@@ -12,7 +14,7 @@ def update(request):
     if not host.access_key == request.POST['key']:
         return HttpResponse('Could not authorize')
     update = Update(host=host, 
-                    from_ip=requst.META.REMOTE_ADDR,
+                    from_ip=request.META['REMOTE_ADDR'],
                     message=request.POST['message'])
     update.save()
     return HttpResponse('1')
