9f9fdf71d05ee88d015bc5c341ada83982527067
Subito <ffheidt@gmail.com>
2012-07-30 16:27:13 +0200

    added MultiDBModelAdmin for mydns administration in djangos admin

---
+++ mydns/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+class MultiDBModelAdmin(admin.ModelAdmin):
+    using = 'mydns'
+    
+    def save_model(self, request, obj, form, change):
+        obj.save(using=self.using)
+        
+    def delete_model(self, request, obj):
+        obj.delete(using=self.using)
+            
+    def queryset(self, request):
+        return super(MultiDBModelAdmin, self).queryset(request).using(self.using)
+                
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        return super(MultiDBModelAdmin, self).formfield_for_foreignkey(db_field, request=request, using=self.using, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        return super(MultiDBModelAdmin, self).formfield_for_manytomany(db_field, request=request, using=self.using, **kwargs)
